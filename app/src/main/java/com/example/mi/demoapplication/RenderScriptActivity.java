5886d6d30701d8dc7952d89bb9f0750b4aa30661
chalilayang <chalilayang@gmail.com>
2020-10-08 20:52:56 +0800

    renderscript

--- app/src/main/java/com/example/mi/demoapplication/RenderScriptActivity.java
+++ app/src/main/java/com/example/mi/demoapplication/RenderScriptActivity.java
@@ -2,6 +2,8 @@ package com.example.mi.demoapplication;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.BitmapRegionDecoder;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.renderscript.Allocation;
 import android.renderscript.RenderScript;
@@ -12,6 +14,8 @@ import android.widget.ImageView;
 import com.example.mi.ScriptC_flip;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+
+import java.io.InputStream;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -25,6 +29,7 @@ public class RenderScriptActivity extends AppCompatActivity {
     ImageView image1;
     private RenderScript mRenderScript;
     private ScriptC_flip mScriptCFlip;
+    private Bitmap[] mBitmaps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,14 +38,14 @@ public class RenderScriptActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        mBitmaps = decodeRegion();
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-                Bitmap flipBitmap = flipBitmap();
+                Bitmap flipBitmap = processBitmap2();
                 image1.setImageBitmap(flipBitmap);
             }
         });
@@ -63,7 +68,7 @@ public class RenderScriptActivity extends AppCompatActivity {
         Bitmap outBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
         if (mOutAllocation == null) {
             mOutAllocation = Allocation.createTyped(
-                    mRenderScript, mInAllocation.getType(),Allocation.USAGE_SCRIPT);
+                    mRenderScript, mInAllocation.getType(), Allocation.USAGE_SCRIPT);
         }
         if (mExtraAllocation == null) {
             mExtraAllocation = Allocation.createFromBitmapResource(mRenderScript, getResources(), R.drawable.wechat2);
@@ -72,8 +77,65 @@ public class RenderScriptActivity extends AppCompatActivity {
         mScriptCFlip.set_gIn(mExtraAllocation);
         mScriptCFlip.set_imageHeight(height);
         mScriptCFlip.set_imageWidth(width);
-        mScriptCFlip.forEach_flip(mInAllocation, mOutAllocation);
+        mScriptCFlip.forEach_addPixel(mInAllocation, mOutAllocation);
         mOutAllocation.copyTo(outBitmap);
         return outBitmap;
+    }
+
+    public Bitmap processBitmap() {
+        if (mInAllocation == null) {
+            mInAllocation = Allocation.createFromBitmapResource(mRenderScript, getResources(), R.drawable.wechat2);
+        }
+        Allocation outputAllocation = Allocation.createTyped(
+                mRenderScript, mInAllocation.getType(), Allocation.USAGE_SCRIPT);
+        mScriptCFlip.invoke_process(mInAllocation, outputAllocation);
+        int width = outputAllocation.getType().getX();
+        int height = outputAllocation.getType().getY();
+        Bitmap outBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        outputAllocation.copyTo(outBitmap);
+        return outBitmap;
+    }
+
+    public Bitmap processBitmap2() {
+        long start = System.currentTimeMillis();
+        if (mInAllocation == null) {
+            mInAllocation = Allocation.createFromBitmap(mRenderScript, mBitmaps[0]);
+        }
+        if (mExtraAllocation == null) {
+            mExtraAllocation = Allocation.createFromBitmap(mRenderScript, mBitmaps[1]);
+        }
+        Allocation outputAllocation = Allocation.createTyped(
+                mRenderScript, mInAllocation.getType(), Allocation.USAGE_SCRIPT);
+        int width = mInAllocation.getType().getX();
+        int height = mInAllocation.getType().getY();
+        mScriptCFlip.invoke_process2(mInAllocation, mExtraAllocation, outputAllocation);
+        Bitmap outBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        outputAllocation.copyTo(outBitmap);
+        Log.i(TAG, "processBitmap2: cost " + (System.currentTimeMillis() - start));
+        return outBitmap;
+    }
+
+    public Bitmap[] decodeRegion() {
+        Bitmap[] result = new Bitmap[2];
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(getResources(), R.drawable.screenshot, options);
+        int width = options.outWidth;
+        int height = options.outHeight;
+        try {
+            InputStream inputStream = getResources().openRawResource(R.drawable.screenshot);
+            BitmapRegionDecoder decoder = BitmapRegionDecoder.newInstance(inputStream,false);
+            Rect rect = new Rect(0, 0, width, height / 4);
+            BitmapFactory.Options regionOptions = new BitmapFactory.Options();
+            regionOptions.inPreferredConfig = Bitmap.Config.RGB_565;
+            Bitmap result1 = decoder.decodeRegion(rect, regionOptions);
+            rect.offset(0, 100);
+            Bitmap result2 = decoder.decodeRegion(rect, regionOptions);
+            result[0] = result1;
+            result[1] = result2;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 }
--- app/src/main/rs/com/example/mi/flip.rs
+++ app/src/main/rs/com/example/mi/flip.rs
@@ -8,18 +8,58 @@ uint32_t imageHeight;
 
 uchar4 RS_KERNEL flip(const uchar4 in, uint32_t x, uint32_t y) {
     uchar4 fliped = in;
-    if (y < imageHeight / 3) {
-        fliped = rsGetElementAt_uchar4(gIn, x, imageHeight - y);
+    if (y < imageHeight / 2) {
+        fliped = rsGetElementAt_uchar4(gIn, x, y);
     } else {
         fliped = in;
     }
     return fliped;
 }
 
+uchar4 RS_KERNEL addPixel(const uchar4 in, uint32_t x, uint32_t y) {
+    uchar4 fliped = rsGetElementAt_uchar4(gIn, x, imageHeight - y);
+    uchar4 result = fliped;
+    result.r = in.r + (fliped.r - in.r) / 2;
+    result.g = in.g + (fliped.g - in.g) / 2;
+    result.b = in.b + (fliped.b - in.b) / 2;
+    return result;
+}
 
 void flip_setup(rs_allocation alocationIn, rs_allocation alocationOut){
     imageWidth = rsAllocationGetDimX(alocationIn);
     imageHeight = rsAllocationGetDimY(alocationIn);
     gIn = alocationIn;
     gOut = alocationOut;
+}
+
+static const float4 weight = {0.299f, 0.587f, 0.114f, 0.0f};
+
+uchar4 RS_KERNEL invert(uchar4 in, uint32_t x, uint32_t y) {
+    uchar4 out = in;
+    out.r = 255 - in.r;
+    out.g = 255 - in.g;
+    out.b = 255 - in.b;
+    return out;
+}
+
+uchar4 RS_KERNEL greyscale(uchar4 in) {
+    const float4 inF = rsUnpackColor8888(in);
+    const float val = dot(inF, weight);
+    const float4 outF = (float4){val, val, val, val};
+    return rsPackColorTo8888(inF);
+}
+
+void process(rs_allocation inputImage, rs_allocation outputImage) {
+    const uint32_t imageWidth = rsAllocationGetDimX(inputImage);
+    const uint32_t imageHeight = rsAllocationGetDimY(inputImage);
+    rs_allocation tmp = rsCreateAllocation_uchar4(imageWidth, imageHeight);
+    rsForEach(invert, inputImage, tmp);
+    rsForEach(greyscale, tmp, outputImage);
+}
+
+void process2(rs_allocation inputImage, rs_allocation inputExtraImage, rs_allocation outputImage) {
+    imageWidth = rsAllocationGetDimX(inputImage);
+    imageHeight = rsAllocationGetDimY(inputImage);
+    gIn = inputExtraImage;
+    rsForEach(addPixel, inputImage, outputImage);
 }
