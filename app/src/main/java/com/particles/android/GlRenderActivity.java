a9456614d8a855db918afc2760d6891216a1e517
chalilayang <chalilayang@gmail.com>
2021-04-12 11:42:47 +0800

    代码整理

--- app/src/main/java/com/particles/android/GlRenderActivity.java
+++ app/src/main/java/com/particles/android/GlRenderActivity.java
@@ -1,23 +1,14 @@
 package com.particles.android;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.FrameLayout;
-import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.mi.demoapplication.R;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class GlRenderActivity extends AppCompatActivity {
-
-    @BindView(R.id.button)
-    Button button;
-    @BindView(R.id.image)
-    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
